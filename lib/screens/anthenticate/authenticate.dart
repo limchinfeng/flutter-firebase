109acf58a5323ac9aa9cc1eba9f87401f88b18b4
supershuaifeng <limchinfeng@gmail.com>
2024-03-03 21:55:03 +0800

    Done anonymous sign in

---
+++ lib/screens/anthenticate/authenticate.dart
@@ -0,0 +1,18 @@
+import 'package:coffee_firebase/screens/anthenticate/sign-in.dart';
+import 'package:flutter/material.dart';
+
+class Authenticate extends StatefulWidget {
+  const Authenticate({super.key});
+
+  @override
+  State<Authenticate> createState() => _AuthenticateState();
+}
+
+class _AuthenticateState extends State<Authenticate> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: SignIn(),
+    );
+  }
+}
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,12 @@
+import 'package:coffee_firebase/screens/anthenticate/authenticate.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  const Wrapper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Authenticate();
+  }
+}
