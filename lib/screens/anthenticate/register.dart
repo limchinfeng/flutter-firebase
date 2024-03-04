65a7ab77dc36c83fc864452d385fda818668b7e7
supershuaifeng <limchinfeng@gmail.com>
2024-03-04 21:26:57 +0800

    Done loading

--- lib/screens/anthenticate/register.dart
+++ lib/screens/anthenticate/register.dart
@@ -1,5 +1,6 @@
 import 'package:coffee_firebase/services/auth.dart';
 import 'package:coffee_firebase/shared/constants.dart';
+import 'package:coffee_firebase/shared/loading.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -14,6 +15,7 @@ class _RegisterState extends State<Register> {
 
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
+  bool loading = false;
 
   // text field state
   String email = '';
@@ -22,7 +24,7 @@ class _RegisterState extends State<Register> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return loading ? Loading() : Scaffold(
       backgroundColor: Colors.brown[100],
       appBar: AppBar(
         backgroundColor: Colors.brown[400],
@@ -69,10 +71,14 @@ class _RegisterState extends State<Register> {
               ElevatedButton(
                 onPressed: () async {
                   if(_formKey.currentState!.validate()) {
+                    setState(() {
+                      loading = true;
+                    });
                     dynamic result = await _auth.registerWithEmailAndPassword(email, password);
                     if(result == null) {
                       setState(() {
                         error = 'please supply a valid email and password';
+                        loading = false;
                       });
                     }
                   }
--- lib/screens/anthenticate/sign-in.dart
+++ lib/screens/anthenticate/sign-in.dart
@@ -1,5 +1,6 @@
 import 'package:coffee_firebase/services/auth.dart';
 import 'package:coffee_firebase/shared/constants.dart';
+import 'package:coffee_firebase/shared/loading.dart';
 import 'package:flutter/material.dart';
 
 class SignIn extends StatefulWidget {
@@ -15,6 +16,7 @@ class _SignInState extends State<SignIn> {
 
   final AuthService _auth = AuthService();
   final _formKey = GlobalKey<FormState>();
+  bool loading = false;
 
   // text field state
   String email = '';
@@ -23,7 +25,7 @@ class _SignInState extends State<SignIn> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return loading ? Loading() : Scaffold(
       backgroundColor: Colors.brown[100],
       appBar: AppBar(
         backgroundColor: Colors.brown[400],
@@ -79,10 +81,14 @@ class _SignInState extends State<SignIn> {
                 ),
                 onPressed: () async {
                   if(_formKey.currentState!.validate()) {
+                    setState(() {
+                      loading = true;
+                    });
                     dynamic result = await _auth.signInWithEmailAndPassword(email, password);
                     if(result == null) {
                       setState(() {
                         error = 'could not sign in with those credentials';
+                        loading = false;
                       });
                     }
                   }
