13e74d75b5a70f9e492ef648ef33b1b27a5393d8
supershuaifeng <limchinfeng@gmail.com>
2024-03-05 18:43:31 +0800

    update data

--- lib/screens/home/settings-form.dart
+++ lib/screens/home/settings-form.dart
@@ -79,9 +79,14 @@ class _SettingsFormState extends State<SettingsForm> {
                     backgroundColor: Colors.pink[400], // Change the color as needed
                   ),
                   onPressed: () async {
-                    print(_currentName);
-                    print(_currentStrength);
-                    print(_currentSugars);
+                    if(_formKey.currentState!.validate()) {
+                      await DatabaseService(uid: user.uid).updateUserData(
+                        _currentSugars ?? userData!.sugars, 
+                        _currentName ?? userData!.name, 
+                        _currentStrength ?? userData!.strength,
+                      );
+                      Navigator.pop(context);
+                    }
                   },
                   child: Text(
                     'Update',
